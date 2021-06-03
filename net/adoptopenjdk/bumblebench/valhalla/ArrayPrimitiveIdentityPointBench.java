c4d20540f314a330c28714efe7f586343c59bc44
Annabelle Huo <Annabelle.Huo@ibm.com>
2021-06-03 14:20:01 -0400

    Add primitive and identity array element load benchmark
    Signed-off-by: Annabelle Huo <Annabelle.Huo@ibm.com>

---
+++ net/adoptopenjdk/bumblebench/valhalla/ArrayPrimitiveIdentityPointBench.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.valhalla;
+
+import net.adoptopenjdk.bumblebench.core.MicroBench;
+
+public final class ArrayPrimitiveIdentityPointBench extends MicroBench {
+   public int numOfIdentityElem = 1000;
+   public int numOfPrimitiveElem = 1;
+	private long _sum;
+
+   public final IdentityPoint[] identityArray = createIdentityArray();
+   public final PrimitivePoint[] primitiveArray = createPrimitiveArray();
+
+   public IdentityPoint[] createIdentityArray() {
+      IdentityPoint[] identityArray = new IdentityPoint[numOfIdentityElem];
+
+      for (int i=0; i < numOfIdentityElem; i++) {
+         identityArray[i] = new IdentityPoint(i,i+1);
+      }
+      return identityArray;
+   }
+
+   public PrimitivePoint[] createPrimitiveArray() {
+      PrimitivePoint[] primitiveArray = new PrimitivePoint[numOfPrimitiveElem];
+
+      for (int i=0; i < numOfPrimitiveElem; i++) {
+         primitiveArray[i] = new PrimitivePoint(i,i+1);
+      }
+      return primitiveArray;
+   }
+
+   public int sum(Point[] arr) {
+      int sum = 0;
+
+      for (int i=0; i < arr.length; i++) {
+         sum += arr[i].getX() + arr[i].getY();
+         }
+
+      return sum;
+   }
+
+	@Override
+   protected long doBatch(long numIterations) throws InterruptedException {
+		for (long i = 0; i < numIterations; i+=1) {
+			_sum = 0;
+         _sum += sum(identityArray);
+		}
+      _sum += sum(primitiveArray);
+
+      return numIterations;
+	}
+}
