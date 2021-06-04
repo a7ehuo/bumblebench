3289612d0d17344bf2cbd562ff10901ecf0431f5
Annabelle Huo <Annabelle.Huo@ibm.com>
2021-06-04 17:11:33 -0400

    Add mixed identity and primitive array benchmark
    Signed-off-by: Annabelle Huo <Annabelle.Huo@ibm.com>

---
+++ net/adoptopenjdk/bumblebench/valhalla/ArrayMixedBench.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+public final class ArrayMixedBench {
+   private static int NUM_ARRAY = 10;
+   private static int ARRAY_LENGTH = 2;
+   private static int PRIMITIVE_ARRAY_INDEX = NUM_ARRAY - 1;
+   private static Object[][] data;
+
+   static {
+      data = new Object[NUM_ARRAY][ARRAY_LENGTH];
+
+      for (int i=0; i < PRIMITIVE_ARRAY_INDEX; i++) {
+         data[i] = new IdentityClass[ARRAY_LENGTH];
+         for (int j=0; j < data[i].length; j++) {
+            data[i][j] = new IdentityClass();
+	      }
+      }
+
+      data[PRIMITIVE_ARRAY_INDEX] = new PrimitiveClass[ARRAY_LENGTH];
+      for (int j=0; j < data[PRIMITIVE_ARRAY_INDEX].length; j++) {
+         data[PRIMITIVE_ARRAY_INDEX][j] = new PrimitiveClass();
+      }
+   }
+
+   @Override
+   protected long doBatch(long numIterations) throws InterruptedException {
+     long numIterForIdentity = (new Double(numIterations * 0.9999)).longValue();
+     // make sure the primitive array is picked at least once in each batch
+     numIterForIdentity = (numIterForIdentity >= numIterations) ? (numIterations - 1) : numIterForIdentity;
+
+     for (long loop = 0; loop < numIterations; loop++) {
+        int index = (int)((loop < numIterForIdentity) ? (loop % PRIMITIVE_ARRAY_INDEX) : PRIMITIVE_ARRAY_INDEX);
+        int length = data[index].length;
+
+        for (int j=0; j<length/2; j++) {
+            data[index][j] = data[index][length-j-1];
+	      }
+		}
+
+   return numIterations;
+	}
+}
